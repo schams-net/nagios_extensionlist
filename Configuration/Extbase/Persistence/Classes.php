bcf1f0377ebda95271126d34042294cedc896929
Michael Schams <typo3@2020.schams.net>
2020-09-03 15:41:10 +1000

    [CLEANUP] Add missing copyright comments

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -3,7 +3,17 @@ declare(strict_types=1);
 namespace SchamsNet\NagiosExtensionlist;
 
 /*
- * (c)2020 by Michael Schams <schams.net>
+ * This file is part of the TYPO3 Extension "Nagios Extensionlist"
+ *
+ * Author: Michael Schams <schams.net>
+ * Website: https://schams.net
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please see
+ * https://www.gnu.org/licenses/gpl.html
  */
 
 return [
